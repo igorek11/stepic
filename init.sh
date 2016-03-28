730e07c84f0dbc58318f94ddbe11cf7a4747e55c
igorek <pixoid11@gmail.com>
2016-03-28 23:31:42 +0300

    next

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm -rf /etc/nginx/nginx.conf
-ln -s  /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
+ln -sf  /home/box/web/etc/nginx.conf /etc/nginx/nginx.conf
 rm -rf /etc/nginx/sites-enabled/default
-ln -s  /home/box/web/etc/hello.conf /etc/gunicorn.d/hello
+ln -sf  /home/box/web/etc/hello.conf /etc/gunicorn.d/hello
+ln -sf  /home/box/web/etc/ask.conf /etc/gunicorn.d/ask
 /etc/init.d/nginx restart
 /etc/init.d/gunicorn restart
