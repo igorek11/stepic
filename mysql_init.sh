b065ad37e29f0fe0f57a2f1e4d15190084700b8e
igorek11 <pixoid11@gmail.com>
2016-06-01 00:41:18 +0300

    views-next

--- mysql_init.sh
+++ mysql_init.sh
@@ -1,11 +1,12 @@
-pip install pymysql
+#pip install pymysql
 
 #И в settings.py  прописал:
 
 #import pymysql
 
-
-pymysql.install_as_MySQLdb() 
+#pymysql.install_as_MySQLdb() 
+#
+/etc/init.d/mysql start
 mysql -uroot -e "CREATE DATABASE web;"
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY '1234';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON web.* TO 'box'@'localhost';"
